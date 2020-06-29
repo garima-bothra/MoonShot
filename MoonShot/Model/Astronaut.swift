0265dece7ba984dd229f4b8d37b21b953569f956
garima94921 <gaarimabothra@gmail.com>
2020-06-30 05:18:30 +0530

    Astronaut and Mission model added

---
+++ MoonShot/Model/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Garima Bothra on 30/06/20.
+//  Copyright © 2020 Garima Bothra. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ MoonShot/Model/Mission.swift
@@ -0,0 +1,21 @@
+//
+//  Mission.swift
+//  MoonShot
+//
+//  Created by Garima Bothra on 30/06/20.
+//  Copyright © 2020 Garima Bothra. All rights reserved.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
