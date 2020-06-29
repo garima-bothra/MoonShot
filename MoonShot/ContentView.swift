5a15cab3183cc2ac006811b0cf65e925622f6c73
garima94921 <gaarimabothra@gmail.com>
2020-06-29 19:06:27 +0530

    Initial Commit

---
+++ MoonShot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonShot
+//
+//  Created by Garima Bothra on 29/06/20.
+//  Copyright © 2020 Garima Bothra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
