47cf7db7ddb2df33a87ba8a3ae52e7fa39403ad4
garima94921 <gaarimabothra@gmail.com>
2020-06-30 15:17:33 +0530

    Crew members added

--- MoonShot/View/MissionView.swift
+++ MoonShot/View/MissionView.swift
@@ -11,6 +11,13 @@ import SwiftUI
 struct MissionView: View {
     let mission: Mission
 
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+
+    let astronauts: [CrewMember]
+
     var body: some View {
         GeometryReader { geometry in
             ScrollView(.vertical) {
@@ -30,12 +37,29 @@ struct MissionView: View {
         }
         .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
     }
+
+    init(mission: Mission, astronauts: [Astronaut]) {
+        self.mission = mission
+
+        var matches = [CrewMember]()
+
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            } else {
+                fatalError("Missing \(member)")
+            }
+        }
+
+        self.astronauts = matches
+    }
 }
 
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronauts: astronauts)
     }
 }
